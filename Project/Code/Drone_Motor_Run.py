a7c079d60eeb79738ed52660e79f2057bdf692c9
Demo User <debian@beaglebone.localdomain>
2018-11-13 01:31:06 +0000

    Added final comments for Drone_Motor_Run.py and edited the startup shell script to match with function.

--- Project/Code/Drone_Motor_Run.py
+++ Project/Code/Drone_Motor_Run.py
@@ -37,10 +37,11 @@ This program runs all motors on my homebrew drone
 
 Upon startup, the pocket beagle will power on the four motors and run 
 them for 5 seconds before shutting down. This serves as a proof of concept
-fo the drone and the viability of my power/logic setup
+for the drone and the viability of my power/logic setup
 
 This program will also allow for easy control of motor throttle for 
-thrust/weight testing
+thrust/weight testing. By changing the pulse width, the power of the 
+motor can be increased.
 
 """
 # ------------------------------------------------------------------------
@@ -48,6 +49,7 @@ thrust/weight testing
 # ------------------------------------------------------------------------
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM as PWM
+import time
 # ------------------------------------------------------------------------
 # Constants - N/A
 # ------------------------------------------------------------------------
@@ -62,8 +64,36 @@ import Adafruit_BBIO.PWM as PWM
 # Functions 
 # ------------------------------------------------------------------------
 def main():
-    PWM.start("P1_33",50,2)
-
+    # Arm the motor
+    PWM.start("P1_33",0,100)        # Initialize all the pins with 0 duty cycle
+    PWM.start("P1_36",0,100)
+    PWM.start("P2_1",0,100)
+    PWM.start("P2_3",0,100)
+    time.sleep(1.0)                 # Hold for one second
+    PWM.set_duty_cycle("P1_33",10)  # Start the pins a a l;ow duty cycle
+    PWM.set_duty_cycle("P1_36",10)
+    PWM.set_duty_cycle("P2_1",10)
+    PWM.set_duty_cycle("P2_3",10)
+    time.sleep(1.0)
+   
+    # Run the motor for 5 seconds
+    PWM.set_duty_cycle("P1_33",11) # Run the pins at high duty cycle to
+    PWM.set_duty_cycle("P1_36",11) # start the motors
+    PWM.set_duty_cycle("P2_1",11)
+    PWM.set_duty_cycle("P2_3",11)
+    time.sleep(1.0)
+    PWM.set_duty_cycle("P1_33",14) # Ramp up motors for power/current test
+    PWM.set_duty_cycle("P1_36",14)
+    PWM.set_duty_cycle("P2_1",14)
+    PWM.set_duty_cycle("P2_3",14)
+    time.sleep(5.0)
+    PWM.stop("P1_33")              # Stop PWM signals and clean pins
+    PWM.stop("P1_36")
+    PWM.stop("P2_1")
+    PWM.stop("P2_3")
+    PWM.cleanup
+    
+    
     
 # ------------------------------------------------------------------------
 # Main script
